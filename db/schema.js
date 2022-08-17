ddff5a325cbe5e7c7d1aaecc13e3b146d87af2af
Nathan Loudon <vesnathan@gmail.com>
2022-08-17 16:13:03 +1000

    husky test

---
+++ db/schema.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+require("dotenv").config();
+const mysql = require("mysql2");
+const {
+  _DB_USER,
+  _DB_PASSWORD,
+  _DB_PORT,
+  _DB_HOST,
+  _DB_NAME,
+} = require("../config/config");
+
+const con = mysql.createConnection({
+  host: _DB_HOST,
+  user: _DB_USER,
+  password: _DB_PASSWORD,
+  port: _DB_PORT,
+});
+
+const databaseName = _DB_NAME;
+
+con.connect((err) => {
+  if (err) throw err;
+  con.query(`CREATE DATABASE IF NOT EXISTS ${databaseName}`, (err2) => {
+    if (err2) throw err2;
+  });
+  con.end();
+  console.log("CREATED DATABASE");
+});
