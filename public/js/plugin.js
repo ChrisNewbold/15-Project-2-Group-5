96b2cf5d346c3b2bba33923d32bb5e481ff82c77
Nathan Loudon <vesnathan@gmail.com>
2022-08-18 17:41:23 +1000

    added plugin.js

---
+++ public/js/plugin.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.cssText =
+    "position:fixed; width:100%; height:100%; top: 0; left: 0; z-index:100; background-color: rgba(0,0,0,.8);";
+  overlayDiv.setAttribute("id", "overlay");
+
+  const wrapperDiv = document.createElement("div");
+  wrapperDiv.style.cssText =
+    "position:relative; width:100%; height:100%; z-index:500;";
+
+  const elemIFrame = document.createElement("iFrame");
+  elemIFrame.style.cssText =
+    "position:absolute; width:50%; height:50%; top: 50%; left: 50%; opacity:1; z-index:1000; background:#fff; margin: auto; transform: translate(-50%,-50%)";
+
+  wrapperDiv.appendChild(elemIFrame);
+  overlayDiv.appendChild(wrapperDiv);
+
+  fetch("https://app-techblog.herokuapp.com/api/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      url: window.location.href,
+      b: 2,
+    }),
+  })
+    .then((response) => {
+      document.body.appendChild(overlayDiv);
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Something went wrong");
+    })
+    .then((responseJson) => {
+      // Do something with the response
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
