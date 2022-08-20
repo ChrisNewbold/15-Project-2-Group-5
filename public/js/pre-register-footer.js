534675c10ef869ea5cba47b6a1a69b715c3cbf7f
Nathan Loudon <vesnathan@gmail.com>
2022-08-20 22:52:13 +1000

    fixed routes

---
+++ public/js/pre-register-footer.js
@@ -0,0 +1,30 @@
+function myInitCode() {
+  setTimeout(() => {
+    window.parent.postMessage(
+      document.getElementById("wrapper").offsetHeight + 50
+    );
+  }, 1000);
+  const emailElement = document.getElementById("inputEmail3");
+  const passwordElement = document.getElementById("inputPassword3");
+  const termsElement = document.getElementById("flexCheckDefault1");
+  const privacyElement = document.getElementById("flexCheckDefault2");
+  const buttonElement = document.getElementById("submit-btn");
+  buttonElement.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e);
+    const formData = {
+      email: emailElement.value,
+      password: passwordElement.value,
+      terms: termsElement.checked,
+      privacy: privacyElement.checked,
+    };
+    console.log(formData);
+  });
+}
+if (document.readyState !== "loading") {
+  myInitCode();
+} else {
+  document.addEventListener("DOMContentLoaded", () => {
+    myInitCode();
+  });
+}
