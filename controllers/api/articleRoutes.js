781c17ba61151b88d8fb21abbb4b49155c147aa3
Nathan Loudon <vesnathan@gmail.com>
2022-08-21 12:20:50 +1000

    added path env var to splash template

--- controllers/api/articleRoutes.js
+++ controllers/api/articleRoutes.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
+const { _NODE_ENV } = require("../../config/config");
 
 router.post("/", (req, res) => {
   try {
-    res.render("pre-register", { layout: "splash" }, (err, html) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        console.log(`ERROR: ${err}`);
+    res.render(
+      "pre-register",
+      { layout: "splash", devPath: _NODE_ENV === "development" },
+      (err, html) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.log(`ERROR: ${err}`);
+        }
+        res.send({ html, id: 1 });
       }
-      res.send({ html, id: 1 });
-    });
+    );
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log(`ERROR: ${err}`);
