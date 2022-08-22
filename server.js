3dcff946ea364c22a9a0472240693d10ec0ff248
Chris Newbold <chris@newboldcreative.com>
2022-08-22 21:56:56 +1000

    update reader-registered setup

--- server.js
+++ server.js
@@ -59,10 +59,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/splashTest2", (req, res) => {
-  res.render("reader-registered", { layout: "main" });
+  res.render("reader-registered", { layout: "splash", justreg: true });
 });
 app.get("/splashTest3", (req, res) => {
-  res.render("reader-outOfCredit", { layout: "main" });
+  res.render("reader-outOfCredit", { layout: "splash" });
 });
 app.use(controllers);
 
