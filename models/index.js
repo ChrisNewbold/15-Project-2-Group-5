d6c9b551180984013e9fe355620e6ba736ef7ee9
Juan Marquez <juanmarquez4@gmail.com>
2022-08-22 18:47:04 +1000

    Created Tags db

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Article = require("./Article");
 const Blogger = require("./Blogger");
 const Reader = require("./Reader");
+const Tag = require("./Tags");
 
 // Reader can have many Articles
 Reader.hasMany(Article, {
@@ -16,4 +17,5 @@ module.exports = {
   Article,
   Blogger,
   Reader,
+  Tag,
 };
