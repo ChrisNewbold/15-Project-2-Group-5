1bdd31c9763730ad0733d65f771ecff4851d738c
Nathan Loudon <vesnathan@gmail.com>
2022-08-23 12:24:47 +1000

    added blogger login form

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,10 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+router.get("/join", (req, res) => {
+  res.render("blogger-join", { layout: "main" });
+});
+
 router.use("/", (req, res) => {
   res.send("403: Forbidden");
 });
