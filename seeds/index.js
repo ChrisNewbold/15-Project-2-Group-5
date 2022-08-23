8f476a1c7a53a67861ad0d1c350e68d3bd446b79
Nathan Loudon <vesnathan@gmail.com>
2022-08-23 15:59:06 +1000

    bloggerTags and Tags seeders done

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const seedArticles = require("./article-seed");
 const seedBlogger = require("./blogger-seed");
 const seedReader = require("./reader-seed");
+const seedTags = require("./tag-seed");
+const seedBloggerTags = require("./bloggerTag-seed");
 const sequelize = require("../config/connection");
 
 const seedAll = async () => {
@@ -15,6 +17,10 @@ const seedAll = async () => {
   console.log("\n----- BLOGGER SEEDED -----\n");
   await seedReader();
   console.log("\n----- READER SEEDED -----\n");
+  await seedTags();
+  console.log("\n----- TAGS SEEDED -----\n");
+  await seedBloggerTags();
+  console.log("\n----- BLOGGER-TAGS SEEDED -----\n");
 
   process.exit(0);
 };
