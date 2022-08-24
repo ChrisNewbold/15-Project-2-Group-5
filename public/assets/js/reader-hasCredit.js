bf8fca4f4ac2ed228b48afe188cd7adc13ece4b8
Nathan Loudon <vesnathan@gmail.com>
2022-08-24 19:38:30 +1000

    added error catch to blogger add url form

--- public/assets/js/reader-hasCredit.js
+++ public/assets/js/reader-hasCredit.js
@@ -5,6 +5,12 @@ function hasCreditHbrsCode() {
       height: document.getElementById("wrapper").offsetHeight + 70,
     });
   }, 1000);
+  document.getElementById("readArticle").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.parent.postMessage({
+      splash: "reader-hasCredit2",
+    });
+  });
 }
 if (document.readyState !== "loading") {
   hasCreditHbrsCode();
