a0de69c19dd2b06273830bee0f9aa64020968348
Nathan Loudon <vesnathan@gmail.com>
2022-08-24 19:06:05 +1000

    added error catch to blogger add url form

--- controllers/index.js
+++ controllers/index.js
@@ -25,30 +25,34 @@ router.get("/reader-join", (req, res) => {
   res.render("reader-join", { layout: "main" });
 });
 router.get("/dashboard", async (req, res) => {
-  let articleData;
-  if (req.session.userTypeBlogger) {
-    articleData = await Article.findAll({
-      where: { blogger_id: req.session.userId },
+  if (!req.session.userAuthenticated) {
+    res.redirect("/");
+  } else {
+    let articleData;
+    if (req.session.userTypeBlogger) {
+      articleData = await Article.findAll({
+        where: { blogger_id: req.session.userId },
+      });
+    }
+    if (req.session.userTypeReader) {
+      articleData = await Article.findAll();
+    }
+
+    const serializedArticleData = articleData.map((article) =>
+      article.get({ plain: true })
+    );
+    console.log("serializedArticleData", serializedArticleData);
+    res.render("dashboard", {
+      layout: "main",
+      data: {
+        userFirstName: req.session.userFirstName,
+        userTypeReader: req.session.userTypeReader,
+        userTypeBlogger: req.session.userTypeBlogger,
+        userAuthenticated: req.session.userAuthenticated,
+        articleData: serializedArticleData,
+      },
     });
   }
-  if (req.session.userTypeReader) {
-    articleData = await Article.findAll();
-  }
-
-  const serializedArticleData = articleData.map((article) =>
-    article.get({ plain: true })
-  );
-  console.log("serializedArticleData", serializedArticleData);
-  res.render("dashboard", {
-    layout: "main",
-    data: {
-      userFirstName: req.session.userFirstName,
-      userTypeReader: req.session.userTypeReader,
-      userTypeBlogger: req.session.userTypeBlogger,
-      userAuthenticated: req.session.userAuthenticated,
-      articleData: serializedArticleData,
-    },
-  });
 });
 
 router.get("/", (req, res) => {
