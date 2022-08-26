fd0cc7ce1f47ecfc6dd28e3efa9562cddfd0488c
Nathan Loudon <vesnathan@gmail.com>
2022-08-26 23:02:54 +1000

    cors

--- server.js
+++ server.js
@@ -18,17 +18,15 @@ const myStore = new SequelizeStore({
   db: sequalize,
 });
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
 app.use(
   session({
     secret: _SESSION_SECRET,
